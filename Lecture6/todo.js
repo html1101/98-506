1b632516becb372d4992c3787e91c0a4c8978b19
Sarah Cross <sarahcro@andrew.cmu.edu>
2025-09-30 18:23:26 -0400

    added enter

--- Lecture6/todo.js
+++ Lecture6/todo.js
@@ -7,18 +7,25 @@ const addElement = (todoName) => {
 
     // Create an element for our item
     // see the sample element; it will have
-    // a class item, and within it a checkbox and
+    // a class "item", and within it a checkbox and
     // the todo list name.
 
-    // Add our todo to our list:
+    // Add our todo to our DOM:
 
 }
 
-// Listen for when we press add-elem to add to our todo list:
-document.getElementById("add-elem").addEventListener("click", () => {
+const addTodo = () => {
     // Get todo-name's content
     const todoName = document.getElementById("todo-name").value;
 
     // Add it to the todo list!
     addElement(todoName);
+}
+
+// Listen for when we press add-elem to add to our todo list:
+document.getElementById("add-elem").addEventListener("click", addTodo);
+document.getElementById("todo-name").addEventListener("keydown", (evt) => {
+    if(evt.key == "Enter") {
+        addTodo();
+    }
 });
